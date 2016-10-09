ea5d8714bf94650a0a3607a24ac813fc531fa0df
hanocha <saiki@feedforce.jp>
2016-10-09 13:14:07 +0900

    add modifing sudoers code in user.rb

--- cookbooks/user.rb
+++ cookbooks/user.rb
@@ -4,9 +4,22 @@ user 'hanocha' do
   password '$6$qwerty$w.vCCevZckrG5L5P7koOVQZxROGBMEc5lrUCK63IQnMIVo31D1EZRMv8j5BcU5F3/4FKr3u32KOwWHT2GxBJL1'
 end
 
-template "/etc/sudoers" do
+package "vim" do
+  action :install
+end
+
+package "git" do
+  action :install
+end
+
+template "/home/vagrant/sudoers.old" do
   source "templates/sudoers"
-  mode   "440"
-  owner  "root"
-  group  "root"
+end
+
+execute "convert lf code dos to unix" do
+  command "perl -pe 's/\r//' /home/vagrant/sudoers.old > /home/vagrant/sudoers"
+end
+
+execute "copy sudoers file to /etc/sudoers" do
+  command "cp /home/vagrant/sudoers /etc/sudoers"
 end
