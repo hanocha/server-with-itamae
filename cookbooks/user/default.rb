214423a026d9ea689f3b6ab3506106356c5414f2
hanocha <saiki@feedforce.jp>
2016-10-10 15:39:57 +0900

    set default shell zsh

--- cookbooks/user/default.rb
+++ cookbooks/user/default.rb
@@ -3,3 +3,8 @@ user 'hanocha' do
   gid 'wheel'
   password '$6$qwerty$w.vCCevZckrG5L5P7koOVQZxROGBMEc5lrUCK63IQnMIVo31D1EZRMv8j5BcU5F3/4FKr3u32KOwWHT2GxBJL1'
 end
+
+execute 'set default shell to zsh' do
+  command 'chsh -s /bin/zsh hanocha'
+  only_if 'chsh -l | grep /bin/zsh'
+end
